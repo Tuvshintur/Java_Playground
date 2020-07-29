d55991e6558c721e12a1e9cc6936ccdae9e8a5cb
Tuvshintur Batzorig <tuvshinturbatzorig@gmail.com>
2020-07-29 15:55:10 -0500

    java starting

---
+++ demo/src/main/java/com/example/demo/config/AppConfig.java
@@ -0,0 +1,19 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@EnableWebMvc
+@Configuration
+public class AppConfig {
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver  viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("templates/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+}
